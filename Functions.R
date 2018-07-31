b3a0a9aba3c002a788869a18bed7fc8bda8552c0
githubmao <maoyanbit@foxmail.com>
2018-07-31 19:42:57 +0800

    ini file input functions

--- Functions.R
+++ Functions.R
@@ -1,5 +1,6 @@
 #-------------- Code Description ----------------------------------------------#
 # Description: 导入.ini数据，并将其整理成需要的数据格式，需要的函数。
+# Notes: ver1.0, 20180510, By MaoYan;
 #
 # Notes:
 #   GetDayoftheWeek: 获取事件发生的时间，星期几。
@@ -12,191 +13,256 @@
 
 # 1.计算事件发生的时间，星期几----
 GetDayoftheWeek <- function(kInputDay) {
-  # Input:
-  #      kInputDay: 原始数据星期几。
-  # Output:输出数据星期几。
+  # 输入: kInputDay: 原始数据星期几。
+  # 输出: 输出数据星期几。
   
   if (kInputDay == "一") {         # 星期一
-    return("Mon.")
+    return("Mon")
   } else if (kInputDay == "二") {  # 星期二
-    return("Tues.")
+    return("Tues")
   } else if (kInputDay == "三") {  # 星期三
-    return("Wed.")
+    return("Wed")
   } else if (kInputDay == "四") {  # 星期四
-    return("Thur.")
+    return("Thur")
   } else if (kInputDay == "五") {  # 星期五
-    return("Fri.")
+    return("Fri")
   } else if (kInputDay == "六") {  # 星期六
-    return("Sat.")
+    return("Sat")
   } else if (kInputDay == "日") {  #星期日
-    return("Sun.")
+    return("Sun")
   } else {
-    return("DataError")  # 其他情况
+    stop("Error 'day of the week'!") # 其他情况
   }
 }
 
 
 # 2. 计算事件发生的时间，月份----
 GetMonth <- function(kInputMonth) {
-  # Input:
-  #      kInputMonth：原始数据月份。
-  # Output:输出数据月份。
+  # 输入: kInputMonth：原始数据月份。
+  # 输出: 输出数据月份。
   
   if (kInputMonth == "一月") {  # 一月
-    return("Jan.")
+    return("Jan")
   } else if (kInputMonth == "二月") {  # 二月
-    return("Feb.")
+    return("Feb")
   } else if (kInputMonth == "三月") {  # 三月
-    return("Mar.")
+    return("Mar")
   } else if (kInputMonth == "四月") {  #  四月
-    return("Apr.")
+    return("Apr")
   } else if (kInputMonth == "五月") {  # 五月
-    return("May.")
+    return("May")
   } else if (kInputMonth == "六月") {  # 六月
-    return("Jun.")
+    return("Jun")
   } else if (kInputMonth == "七月") {  # 七月
-    return("Jul.")
+    return("Jul")
   } else if (kInputMonth == "八月") {  # 八月
-    return("Aug.")
+    return("Aug")
   } else if (kInputMonth == "九月") {  # 九月
-    return("Sep.")
+    return("Sep")
   } else if (kInputMonth == "十月") {  # 十月
-    return("Oct.")
+    return("Oct")
   } else if (kInputMonth == "十一月") {  # 十一月
-    return("Nov.")
+    return("Nov")
   } else if (kInputMonth == "十二月") {  # 十二月
-    return("Dec.")
+    return("Dec")
   } else {
-    return("DataError")  # 其他情况
+    stop("Error 'Month'!")  # 其他情况
   }
 }
 
 
 # 3.获取ini文件中的Basic Information----
 GetBasicInformation <- function(tFile) {
-  # Input: 读取的ini文件。
-  # Output: 含Basic Information各项信息的数据框。
+  # 输入: 读取的ini文件。
+  # 输出: 含Basic Information各项信息的数据框。
   
-  # BI, recordDate----
-  if (!is.null(tfile$`Basic Information`$Record_Date)) {
+  list.BI <- tFile$`Basic Information`  # BasicInformation
   
-    tmp.recorddate <- tfile$`Basic Information`$Record_Date  # 原始recordDate
-    tmp.dayoftheweek <- GetDayoftheWeek(strsplit(tmp.recorddate,
-                                                 split = ",")[[1]][1])  # 星期几
-    tmp.month <- GetMonth(strsplit(strsplit(tmp.recorddate,
-                                            split = ",")[[1]][2],
-                                   split = " ")[[1]][2])  # 月份
-    tmp.date <- strsplit(strsplit(tmp.recorddate, split = ",")[[1]][2],
-                         split = " ")[[1]][3]  # 日
-    tmp.year <- strsplit(strsplit(tmp.recorddate, split = ",")[[1]][3],
-                         split = " ")[[1]][2]  # 年
+  # BI, Record_Date----
+  # 1.Record_Date不为NULL
+  if (!is.null(list.BI$Record_Date)) {
+    
+    tmp.recdate <- strsplit(x = list.BI$Record_Date,
+                            split = ",")[[1]]
+    
+    # 1.1 Record_Date不为NULL, Record_Date正常取值
+    if (tmp.recdate[1] %in% c("一", "二", "三", "四", "五", "六", "日")) {
+      tmp.dayoftheweek <- GetDayoftheWeek(tmp.recdate[1])  # 星期几
+      tmp.dmonth <- strsplit(x = tmp.recdate[2], split = " ")[[1]][2] %>%
+        GetMonth()  # 月份
+      tmp.ddate <- strsplit(x = tmp.recdate[2], split = " ")[[1]][3]  # 日
+      tmp.dyear <- strsplit(x = tmp.recdate[3], split = " ")[[1]][2]  # 年
+      
+    # 1.2 Record_Date不为NULL, Record_Date非正常值
+    } else {
+      tmp.dayoftheweek <- NA
+      tmp.dmonth <- NA
+      tmp.ddate <- NA
+      tmp.dyear <- NA
+    }
+    
+  # 2.Record_Date为NULL
   } else {
     tmp.dayoftheweek <- NA
-    tmp.month <- NA
-    tmp.date <- NA
-    tmp.year <- NA
+    tmp.dmonth <- NA
+    tmp.ddate <- NA
+    tmp.dyear <- NA
   }
   
-  # BI, eventTime----
-  if (!is.null(tfile$`Basic Information`$Time_Of_Event)) {
+  
+  # BI, Time_Of_Event----
+  # 1. Time_Of_Event不为NULL
+  if (!is.null(list.BI$Time_Of_Event)) {
     
-    tmp.eventtime <- tfile$`Basic Information`$Time_Of_Event  # 原始eventTime
-    tmp.time <- strsplit(tmp.eventtime, split = " ")[[1]][1]  # 时间，12h
-    tmp.timehour <- strsplit(tmp.time, split = ":")[[1]][1]  # 时间，12h，时
-    tmp.timeminute <- strsplit(tmp.time, split = ":")[[1]][2]  # 时间，12h，分
-    tmp.ampm <- ifelse(strsplit(tmp.eventtime, split = " ")[[1]][2] == "上午",
-                       "A.M.", "P.M.")  # 时段
-    tmp.timezone <- strsplit(tmp.eventtime, split = " ")[[1]][3]  # 时区
+    tmp.evnttime <- strsplit(x = list.BI$Time_Of_Event,
+                             split = " ")[[1]]
+    
+    # 1.1 Time_Of_Event不为NULL, Time_Of_Event正常取值
+    if (length(tmp.evnttime == 3)) {
+      tmp.timehr <- strsplit(x = tmp.evnttime[1], split = ":")[[1]][1]  # 时
+      tmp.timemin <- strsplit(x = tmp.evnttime[1], split = ":")[[1]][2]  # 分
+      tmp.ampm <- ifelse(tmp.evnttime[2] == "上午", "am", "pm")  # 上午or下午
+      tmp.timezone <- tmp.evnttime[3]  # 时区
+      
+    # 1.2 Time_Of_Event不为NULL，Time_Of_Event非正常值
+    } else {
+      tmp.timehr <- NA
+      tmp.timemin <- NA
+      tmp.ampm <- NA
+      tmp.timezone <- NA
+    }
+    
+  # 2. Time_Of_Event为NULL
   } else {
-    tmp.timehour <- NA
-    tmp.timeminute <- NA
+    tmp.timehr <- NA
+    tmp.timemin <- NA
     tmp.ampm <- NA
     tmp.timezone <- NA
   }
   
-  # BI, eventTrigger----
-  tmp.eventtrigger <- ifelse(!is.null(tfile$`Basic Information`$Event_Trigger),
-                             tfile$`Basic Information`$Event_Trigger,
-                             NA)
   
-  # BI, maxForwardTForce----
-  tmp.maxforwardtforce <- ifelse(!is.null(tfile$`Basic Information`$Max_Forward_T_Force),
-                                 tfile$`Basic Information`$Max_Forward_T_Force,
-                                 NA)
+  # BI, Event_Trigger----
+  tmp.evnttrigger <- ifelse(is.null(list.BI$Event_Trigger), NA,
+                            list.BI$Event_Trigger)
   
-  # BI, maxLateralTForce----
-  tmp.maxlateraltforce <- ifelse(!is.null(tfile$`Basic Information`$Max_Lateral_T_Force),
-                                 tfile$`Basic Information`$Max_Lateral_T_Force,
-                                 NA)
   
-  # BI, triggerSpeed----
-  tmp.triggerspeedKMH <- ifelse(!is.null(strsplit(tfile$`Basic Information`$Speed,
-                                                  split = " ")[[1]][1]),
-                                strsplit(tfile$`Basic Information`$Speed,
-                                         split = " ")[[1]][1],
-                                NA)
+  # BI, Max_Forward_T_Force----
+  tmp.maxfwdforce <- ifelse(is.null(list.BI$Max_Forward_T_Force), NA,
+                            list.BI$Max_Forward_T_Force)
   
-  # BI, moveDirection----
-  tmp.moveDirection <- ifelse(!is.null(tfile$`Basic Information`$Heading),
-                              tfile$`Basic Information`$Heading,
-                              NA)
+  # BI, Max_Lateral_T_Force----
+  tmp.maxlatforce <- ifelse(is.null(list.BI$Max_Lateral_T_Force), NA,
+                            list.BI$Max_Lateral_T_Force)
   
-  # BI, locationLatitude----
-  tmp.locationlatitude <- ifelse(!is.null(tfile$`Basic Information`$Loaction_N),
-                                 tfile$`Basic Information`$Loaction_N,
-                                 NA)
   
-  # BI, locationLongitude----
-  tmp.locationlongitude <- ifelse(!is.null(tfile$`Basic Information`$Loaction_E),
-                                  tfile$`Basic Information`$Loaction_E,
-                                  NA)
+  # BI, Speed----
+  # 1. Speed不为NULL
+  if (!is.null(list.BI$Speed)) {
+    
+    tmp.speedGPS <- strsplit(x = list.BI$Speed, split = " ")[[1]]
+    
+    # 1.1 Speed不为NULL，Speed正常取值
+    if (length(tmp.speedGPS) == 5) {
+      tmp.triggerspeed <- tmp.speedGPS[1]
+      
+    # 1.2 Speed不为NULL，Speed非正常值
+    } else {
+      tmp.triggerspeed <- NA
+    }
+    
+  # 2. Speed为NULL
+  } else {
+    tmp.triggerspeed <- NA
+  }
   
-  # BI, serialNumber----
-  tmp.serialnumber <- ifelse(!is.null(tfile$`Basic Information`$Serial_Number),
-                             tfile$`Basic Information`$Serial_Number,
-                             NA)
   
-  # BI, firmwareVersion----
-  tmp.firmwareversion <- ifelse(!is.null(tfile$`Basic Information`$Firmware_Version),
-                                tfile$`Basic Information`$Firmware_Version,
-                                NA)
+  # BI, Heading----
+  tmp.movedir <- ifelse(is.null(list.BI$Heading), NA,
+                        list.BI$Heading)
   
-  # BI, forwardThreshold----
-  tmp.forwardthreshold <- ifelse(!is.null(tfile$`Basic Information`$Forward_Threshold),
-                                 tfile$`Basic Information`$Forward_Threshold,
-                                 NA)
   
-  # BI, lateralThreshold----
-  tmp.lateralthreshold <- ifelse(!is.null(tfile$`Basic Information`$Lateral_Threshold),
-                                 tfile$`Basic Information`$Lateral_Threshold,
-                                 NA)
+  # BI, Loaction_N----
+  tmp.loclat <- ifelse(is.null(list.BI$Loaction_N), NA,
+                       list.BI$Loaction_N)
   
-  # BI, shockThreshold----
-  tmp.shockthreshold <- ifelse(!is.null(tfile$`Basic Information`$Shock_Threshold),
-                               tfile$`Basic Information`$Shock_Threshold,
-                               NA)
   
-  return(data.frame(dDayoftheWeek = c(tmp.dayoftheweek),
-                    dMonth = c(tmp.month),
-                    dDate = c(tmp.date),
-                    dYear = c(tmp.year),
-                    timeHour = c(tmp.timehour),
-                    timeMinute = c(tmp.timeminute),
-                    timeAMPM = c(tmp.ampm),
-                    timeTimeZone = c(tmp.timezone),
-                    evetTrigger = c(tmp.eventtrigger),
-                    maxForwardTForce = c(tmp.maxforwardtforce),
-                    maxLateralTForce = c(tmp.maxlateraltforce),
-                    triggerSpeedKMH = c(tmp.triggerspeedKMH),
-                    moveDirection = c(tmp.moveDirection),
-                    locationLatitude = c(tmp.locationlatitude),
-                    locationLongitude = c(tmp.locationlongitude),
-                    serialNumber = c(tmp.serialnumber),
-                    firmwareVersion = c(tmp.firmwareversion),
-                    forwardThreshold = c(tmp.forwardthreshold),
-                    lateralThreshold = c(tmp.lateralthreshold),
-                    shockThreshold = c(tmp.shockthreshold)))
+  # BI, Loaction_E----
+  tmp.loclong <- ifelse(is.null(list.BI$Loaction_E), NA,
+                        list.BI$Loaction_E)
+  
+  
+  # BI, Serial_Number----
+  tmp.serialnum <- ifelse(is.null(list.BI$Serial_Number), NA,
+                          list.BI$Serial_Number)
+  
+  
+  # BI, Firmware_Version----
+  tmp.firmwarever <- ifelse(is.null(list.BI$Firmware_Version), NA,
+                            list.BI$Firmware_Version)
+  
+  
+  # BI, Forward_Threshold----
+  tmp.fwdthold <- ifelse(is.null(list.BI$Forward_Threshold), NA,
+                         list.BI$Forward_Threshold)
+  
+  
+  # BI, Lateral_Threshold----
+  tmp.latthold <- ifelse(is.null(list.BI$Lateral_Threshold), NA,
+                         list.BI$Lateral_Threshold)
+  
+  
+  # BI, Shock_Threshold----
+  tmp.shockthold <- ifelse(is.null(list.BI$Shock_Threshold), NA,
+                           list.BI$Shock_Threshold)
+  
+  
+  return(data.frame(dDayoftheWeek = tmp.dayoftheweek,
+                    dMonth = tmp.dmonth,
+                    dDate = tmp.ddate,
+                    dYear = tmp.dyear,
+                    timeHr = tmp.timehr,
+                    timeMin = tmp.timemin,
+                    timeAMPM = tmp.ampm,
+                    timeZone = tmp.timezone,
+                    evntTrigger = tmp.evnttrigger,
+                    maxFwdForce = tmp.maxfwdforce,
+                    maxLatForce = tmp.maxlatforce,
+                    triggerSpeedKMH = tmp.triggerspeed,
+                    moveDir = tmp.movedir,
+                    locLat = tmp.loclat,
+                    locLong = tmp.loclong,
+                    serialNum = tmp.serialnum,
+                    firmwareVer = tmp.firmwarever,
+                    fwdThold = tmp.fwdthold,
+                    latThold = tmp.latthold,
+                    shockThold = tmp.shockthold))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # 4. 获取ini文件中的Move Information----
@@ -247,4 +313,4 @@ GetMoveInformation <- function(tfile,
 }
 
 
-                 
+
